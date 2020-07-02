0a11b9064a9471353222ec7090d9e2a17537a80c
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-07-03 02:53:33 +0700

    aa

--- src/component/Experience/ExpCard.jsx
+++ src/component/Experience/ExpCard.jsx
@@ -1,37 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Card } from "react-bootstrap";
 
-function ExpCard(props) {
-    return (
-        <>
-            <div className="post">
-                <div className="header_post">
-                    <img src="https://images.pexels.com/photos/2529973/pexels-photo-2529973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-                </div>
-                <div className="body_post">
-                    <div className="post_content">
-                        <h1>Lorem Ipsum</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda cumque deserunt
-              dolorum ex exercitationem.</p>
-                        <div className="container_infos">
-                            <div className="postedBy">
-                                <span>author</span>
-                John Doe
-              </div>
-                            <div className="container_tags">
-                                <span>tags</span>
-                                <div className="tags">
-                                    <ul>
-                                        <li>design</li>
-                                        <li>front end</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+function ExpCard({ exp }) {
+  return (
+    <div>
+      <article className="card card--1">
+        <div className="card__info-hover">
+          <svg className="card__like" viewBox="0 0 24 24">
+            <path
+              fill="#000000"
+              d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z"
+            />
+          </svg>
+          <div className="card__clock-info">
+            <svg className="card__clock" viewBox="0 0 24 24">
+              <path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+            </svg>
+            <span className="card__time">{exp.duration} hours</span>
+          </div>
+        </div>
+        <div
+          className="card__img"
+          style={{
+            backgroundImage: "url(" + `${exp.pictureURL[0]}` + ")",
+          }}
+        />
+        <a href="#" className="card_link">
+          <div
+            className="card__img--hover"
+            style={{
+              backgroundImage: "url(" + `${exp.pictureURL[0]}` + ")",
+            }}
+          />
+        </a>
+        <div className="card__info">
+          <span className="card__category"> {exp.country}</span>
+          <h3 className="card__title">{exp.title}</h3>
+          <span className="card__by">From {exp.price}/person</span>
+        </div>
+      </article>
+    </div>
+  );
 }
 
 export default ExpCard;
--- src/component/Experience/ExpList.jsx
+++ src/component/Experience/ExpList.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import ExpCard from './ExpCard';
+import React from "react";
+import ExpCard from "./ExpCard";
+import { Container } from "react-bootstrap";
+import Axios from "axios";
+import Slider from "react-slick";
 
 function ExpList(props) {
-    return (
-        <div className="slider-card-list">
-            <ExpCard></ExpCard>
-        </div>
-    );
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  let [expList, setExpList] = React.useState(null);
+  React.useEffect(() => {
+    getExpList();
+  }, []);
+
+  async function getExpList() {
+    try {
+      let res = await Axios.get(
+        "https://airthb-group6.herokuapp.com/experiences"
+      );
+      setExpList(res.data.data);
+    } catch (err) {}
+  }
+  return (
+    <>
+      {expList ? (
+        <Container>
+          <div className="slider-card-list">
+            <Slider {...settings}>
+            
+                {expList.map((exp) => {
+                  return <ExpCard exp={exp}></ExpCard>;
+                })}
+             
+            </Slider>
+          </div>
+        </Container>
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
 
 export default ExpList;
